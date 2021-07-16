8cc4d238113776e305cc2bc73241dde2c57072d4
Amir <86715539+amirparvez@users.noreply.github.com>
2021-07-16 23:26:51 +0530

    Created ObjectValidator, FileHandler, LocalServerFileHandler & AWSS3FileHandler.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 const FileCustodian = require("./lib/index.js");
 
-const newFile = new FileCustodian.File({ url: "./images/image.png" });
-console.log(newFile);
+const custodian = new FileCustodian({ name: "Jack", });
+setTimeout(async () => {
+    await custodian.newFH({ name: "ls-1", isDefault: true, depository: "local-server", base_path: "D:/node/file custodian/depository/", });
+    await custodian.newFH({ name: "s3-1", isDefault: false, depository: "aws-s3" });
+
+    setTimeout(async () => {
+        console.log(custodian);
+    }, 1000);
+}, 1000);
